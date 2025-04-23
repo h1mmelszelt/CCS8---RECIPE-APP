7c94857372daa71ec32dd8bcf3549ff156bb5e5f
raitzyel <115382241+racky918@users.noreply.github.com>
2025-04-23 14:34:05 +0800

    added navbar mobile in get started (still w menu) + removed QR in mobile ver

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -6,6 +6,7 @@ import {
   Input,
   InputGroup,
   InputRightElement,
+  IconButton,
   Button,
   Menu,
   MenuButton,
@@ -20,8 +21,8 @@ import {
   AlertDialogFooter,
 } from "@chakra-ui/react";
 import { SearchIcon, HamburgerIcon, BellIcon, AddIcon } from "@chakra-ui/icons";
-import { FiHome, FiUser } from "react-icons/fi";
-import { Link, useLocation } from "react-router-dom"; // Import useLocation
+import { FiUser, FiHome } from "react-icons/fi";
+import { Link } from "react-router-dom";
 
 function Navbar({ transparent }) {
   const { isOpen, onOpen, onClose } = useDisclosure();
@@ -43,7 +44,6 @@ function Navbar({ transparent }) {
       top="0"
       zIndex="999"
       bg={transparent ? "transparent" : "white"} // Conditional background
-      display={{ base: "none", md: "block" }} // Hide on small screens, show on medium and larger screens
     >
       <Flex
         alignItems="center"
@@ -112,7 +112,211 @@ function Navbar({ transparent }) {
             SIGN UP
           </Button>
         </Flex>
+
+        {/* Hamburger Menu for All Screens */}
+        <Menu>
+          <MenuButton
+            ml="5"
+            as={Button}
+            _active={{ bg: "white" }}
+            bg="transparent"
+            colorScheme="gray"
+          >
+            <HamburgerIcon boxSize={9} color="#FD660B" />
+          </MenuButton>
+          <MenuList>
+            <MenuItem>
+              <Button
+                onClick={onOpen} // Open the pop-up when clicked
+                color="black"
+                _hover={{ bg: "#EAEAEA" }}
+                width="100%"
+              >
+                Profile
+              </Button>
+            </MenuItem>
+            <MenuItem>
+              <Button
+                as={Link}
+                to="/settings"
+                color="black"
+                _hover={{ bg: "#EAEAEA" }}
+                width="100%"
+              >
+                Settings
+              </Button>
+            </MenuItem>
+            <MenuItem>
+              <Button
+                as={Link}
+                to="/"
+                color="black"
+                _hover={{ bg: "#EAEAEA" }}
+                width="100%"
+              >
+                Logout
+              </Button>
+            </MenuItem>
+          </MenuList>
+        </Menu>
       </Flex>
+
+      {/* AlertDialog for "Please Sign In" */}
+      <AlertDialog
+        isOpen={isOpen}
+        leastDestructiveRef={cancelRef}
+        onClose={onClose}
+      >
+        <AlertDialogOverlay>
+          <AlertDialogContent>
+            <AlertDialogHeader fontSize="lg" fontWeight="bold">
+              Please Sign In
+            </AlertDialogHeader>
+            <AlertDialogBody>
+              You need to sign in to access your profile.
+            </AlertDialogBody>
+            <AlertDialogFooter>
+              <Button ref={cancelRef} onClick={onClose}>
+                Close
+              </Button>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialogOverlay>
+      </AlertDialog>
+
+      {/* Navbar for smaller screens */}
+      <Box
+        display={{ base: "flex", md: "none" }}
+        position="fixed"
+        bottom="0"
+        left="0"
+        right="0"
+        bg="white"
+        boxShadow="md"
+        zIndex="999"
+        justifyContent="space-around"
+        alignItems="center"
+        borderTop="2px solid #caced4"
+        py={2}
+      >
+        {/* Home Icon */}
+        <Link to="/home">
+          <Flex direction="column" align="center">
+            <IconButton
+              icon={<FiHome size={30} />}
+              aria-label="Home"
+              variant="ghost"
+              color={location.pathname === "/home" ? "#FD660B" : "black"} // Highlight if on /home
+              _hover={{ bg: "#FFF1E8" }}
+              size="sm"
+            />
+            <Box
+              fontSize="sm"
+              color={location.pathname === "/home" ? "#FD660B" : "black"}
+              mt={1}
+            >
+              Home
+            </Box>
+          </Flex>
+        </Link>
+
+        {/* Search Icon */}
+        <Link to="/explore">
+          <Flex direction="column" align="center">
+            <IconButton
+              icon={<SearchIcon boxSize={6} />}
+              aria-label="Search"
+              variant="ghost"
+              color={location.pathname === "/explore" ? "#FD660B" : "black"} // Highlight if on /explore
+              _hover={{ bg: "#FFF1E8" }}
+              size="sm"
+            />
+            <Box
+              fontSize="sm"
+              color={location.pathname === "/explore" ? "#FD660B" : "black"}
+              mt={1}
+            >
+              Search
+            </Box>
+          </Flex>
+        </Link>
+
+        {/* Create Icon */}
+        <Link to="/create">
+          <Flex direction="column" align="center">
+            <IconButton
+              icon={<AddIcon boxSize={4} />}
+              aria-label="Create"
+              bg="white"
+              color={location.pathname === "/create" ? "#FD660B" : "black"} // Highlight if on /create
+              border="2px solid"
+              borderColor={
+                location.pathname === "/create" ? "#FD660B" : "black"
+              }
+              _hover={{ bg: "#FFF1E8" }}
+              size="sm"
+            />
+            <Box
+              fontSize="sm"
+              color={location.pathname === "/create" ? "#FD660B" : "black"}
+              mt={1}
+            >
+              Create
+            </Box>
+          </Flex>
+        </Link>
+
+        {/* Notification Icon */}
+        <Link to="/notifications">
+          <Flex direction="column" align="center">
+            <IconButton
+              icon={<BellIcon boxSize={8} />}
+              aria-label="Notifications"
+              variant="ghost"
+              color={
+                location.pathname === "/notifications" ? "#FD660B" : "black"
+              } // Highlight if on /notifications
+              _hover={{ bg: "#FFF1E8" }}
+              size="sm"
+            />
+            <Box
+              fontSize="sm"
+              color={
+                location.pathname === "/notifications" ? "#FD660B" : "black"
+              }
+              mt={1}
+            >
+              Inbox
+            </Box>
+          </Flex>
+        </Link>
+
+        {/* User Icon */}
+        <Link to="/profile">
+          <Flex direction="column" align="center">
+            <IconButton
+              icon={<FiUser size={20} />}
+              aria-label="User Profile"
+              variant="ghost"
+              color={location.pathname === "/profile" ? "#FD660B" : "black"} // Highlight if on /profile
+              _hover={{ bg: "#FFF1E8" }}
+              size="sm"
+              borderRadius="full"
+              border="2px solid"
+              borderColor={
+                location.pathname === "/profile" ? "#FD660B" : "black"
+              }
+            />
+            <Box
+              fontSize="sm"
+              color={location.pathname === "/profile" ? "#FD660B" : "black"}
+              mt={1}
+            >
+              Profile
+            </Box>
+          </Flex>
+        </Link>
+      </Box>
     </Box>
   );
 }
