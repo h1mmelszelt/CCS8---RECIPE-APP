7dc800792373a12e4621cd3e824404788ac01260
raitzyel <115382241+racky918@users.noreply.github.com>
2025-04-28 12:29:20 +0800

    me page update

--- frontend/src/pages/MePage.jsx
+++ frontend/src/pages/MePage.jsx
@@ -7,104 +7,117 @@ import {
   Flex,
   Icon,
   Divider,
+  Button,
 } from "@chakra-ui/react";
 import { ChevronRightIcon } from "@chakra-ui/icons";
 import { Link } from "react-router-dom";
 import { FaUserCog, FaSignOutAlt, FaStar, FaUser } from "react-icons/fa";
-import Navbar from "../components/Navbar(Logged)"; // Import the Navbar
+import Navbar from "../components/Navbar(Logged)";
 
 function MePage() {
   return (
-    <Box bg="white" minH="100vh" color="black">
+    <Box bg="gray.100" minH="100vh" color="black">
       {/* Navbar */}
-      <Navbar /> {/* Render the Navbar at the top */}
+      <Navbar />
+
       {/* Page Content */}
-      <Box p={4}>
+      <Box px={{ base: 4, md: 20 }} py={10}>
         {/* User Info Section */}
-        <Flex align="center" mb={6}>
-          <Avatar
-            size="lg"
-            name="User Name"
-            src="/images/default-avatar.jpg" // Replace with actual avatar URL
-            mr={4}
-          />
-          <Box>
-            <Text fontSize="lg" fontWeight="bold" color="black">
-              User Name
-            </Text>
-            <Text fontSize="sm" color="gray.600">
-              @username
-            </Text>
-          </Box>
+        <Flex
+          direction={{ base: "column", md: "row" }}
+          align="center"
+          justify="space-between"
+          bg="gray.50"
+          p={6}
+          borderRadius="md"
+          boxShadow="md"
+          mb={8}
+        >
+          <Flex align="center" mb={{ base: 4, md: 0 }}>
+            <Avatar
+              size="xl"
+              name="User Name"
+              src="/images/default-avatar.jpg" // Replace with actual avatar URL
+              mr={4}
+            />
+            <Box>
+              <Text fontSize="2xl" fontWeight="bold" color="black">
+                User Name
+              </Text>
+              <Text fontSize="sm" color="gray.600">
+                @username
+              </Text>
+            </Box>
+          </Flex>
         </Flex>
 
-        <Divider borderColor="orange.500" mb={6} />
-
         {/* Options Section */}
-        <VStack spacing={4} align="stretch">
-          {/* Profile */}
-          <Flex
-            as={Link}
-            to="/profile"
-            align="center"
-            justify="space-between"
-            p={4}
-            bg="white"
-            border="2px solid orange"
-            borderRadius="md"
-            _hover={{ bg: "orange.100" }}
-          >
-            <Flex align="center">
-              <Icon as={FaUser} color="orange.500" boxSize={5} mr={4} />
-              <Text fontSize="md" fontWeight="medium" color="black">
-                Profile
-              </Text>
+        <Box bg="white" borderRadius="md" boxShadow="md" p={6}>
+          <Text fontSize="lg" fontWeight="bold" mb={4} color="orange.500">
+            Account Options
+          </Text>
+          <VStack spacing={4} align="stretch">
+            {/* Profile */}
+            <Flex
+              as={Link}
+              to="/profile/:id"
+              align="center"
+              justify="space-between"
+              p={4}
+              bg="gray.50"
+              borderRadius="md"
+              _hover={{ bg: "gray.100" }}
+            >
+              <Flex align="center">
+                <Icon as={FaUser} color="orange.500" boxSize={5} mr={4} />
+                <Text fontSize="md" fontWeight="medium" color="black">
+                  Profile
+                </Text>
+              </Flex>
+              <Icon as={ChevronRightIcon} color="gray.500" />
             </Flex>
-            <Icon as={ChevronRightIcon} color="gray.500" />
-          </Flex>
 
-          {/* Settings */}
-          <Flex
-            as={Link}
-            to="/settings"
-            align="center"
-            justify="space-between"
-            p={4}
-            bg="white"
-            border="2px solid orange"
-            borderRadius="md"
-            _hover={{ bg: "orange.100" }}
-          >
-            <Flex align="center">
-              <Icon as={FaUserCog} color="orange.500" boxSize={5} mr={4} />
-              <Text fontSize="md" fontWeight="medium" color="black">
-                Settings
-              </Text>
+            {/* Settings */}
+            <Flex
+              as={Link}
+              to="/settings"
+              align="center"
+              justify="space-between"
+              p={4}
+              bg="gray.50"
+              borderRadius="md"
+              _hover={{ bg: "gray.100" }}
+            >
+              <Flex align="center">
+                <Icon as={FaUserCog} color="orange.500" boxSize={5} mr={4} />
+                <Text fontSize="md" fontWeight="medium" color="black">
+                  Settings
+                </Text>
+              </Flex>
+              <Icon as={ChevronRightIcon} color="gray.500" />
             </Flex>
-            <Icon as={ChevronRightIcon} color="gray.500" />
-          </Flex>
 
-          {/* Logout */}
-          <Flex
-            as={Link}
-            to="/logout"
-            align="center"
-            justify="space-between"
-            p={4}
-            bg="white"
-            border="2px solid orange"
-            borderRadius="md"
-            _hover={{ bg: "orange.100" }}
-          >
-            <Flex align="center">
-              <Icon as={FaSignOutAlt} color="red.500" boxSize={5} mr={4} />
-              <Text fontSize="md" fontWeight="medium" color="black">
-                Logout
-              </Text>
+            {/* Logout */}
+            <Flex
+              as={Link}
+              to="/logout"
+              align="center"
+              justify="space-between"
+              p={4}
+              bg="gray.50"
+              borderRadius="md"
+              _hover={{ bg: "gray.100" }}
+            >
+              <Flex align="center">
+                <Icon as={FaSignOutAlt} color="red.500" boxSize={5} mr={4} />
+                <Text fontSize="md" fontWeight="medium" color="black">
+                  Logout
+                </Text>
+              </Flex>
+              <Icon as={ChevronRightIcon} color="gray.500" />
             </Flex>
-            <Icon as={ChevronRightIcon} color="gray.500" />
-          </Flex>
-        </VStack>
+          </VStack>
+        </Box>
       </Box>
     </Box>
   );
