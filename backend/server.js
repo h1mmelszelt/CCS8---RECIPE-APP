9257beb2b2d8ce8a94b661a790b4421ab5163238
aeto80 <guillaume.suyko@gmail.com>
2025-05-03 22:24:15 +0800

    deplyoing

--- backend/server.js
+++ backend/server.js
@@ -9,11 +9,14 @@ import recipeRoutes from "./routes/recipe.route.js";
 import userRoutes from "./routes/user.route.js";
 import reviewRoutes from "./routes/review.route.js";
 import cors from "cors";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
 
 app.use(cors());
 app.use(express.json());
@@ -22,6 +25,14 @@ app.use("/api/recipes", recipeRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/reviews", reviewRoutes);
 app.use("/api", userRoutes);
+
+if(process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get( "*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html" ));
+  });
+}
 
 app.listen(5000, () => {
   connectDB();
