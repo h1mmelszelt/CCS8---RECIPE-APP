eda306b080f9ef64c63f1276e05307af589203c4
aeto80 <guillaume.suyko@gmail.com>
2025-05-04 13:55:02 +0800

    yes

--- backend/server.js
+++ backend/server.js
@@ -17,8 +17,6 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const __dirname = path.resolve();
-
 app.use(cors());
 app.use(express.json());
 
@@ -27,24 +25,15 @@ app.use("/api/users", userRoutes);
 app.use("/api/reviews", reviewRoutes);
 app.use("/api", userRoutes);
 
-/*
 // Serve static files from the React app
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 app.use(express.static(path.join(__dirname, "../frontend/dist")));
-*/
-app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
-});
-
-/*
 // Catch-all route to serve index.html for React Router
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
-});a
-*/
+});
 
 app.listen(5000, () => {
   connectDB();
