67972ab827467e96f814c673a4c232b34662016f
aeto80 <guillaume.suyko@gmail.com>
2025-05-04 13:06:02 +0800

    fixes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 export default defineConfig({
   plugins: [react()],
@@ -9,6 +10,13 @@ export default defineConfig({
         target: "http://localhost:5000", // Replace with your backend server URL
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
       },
     },
   },
