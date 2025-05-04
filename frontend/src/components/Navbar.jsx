f9cf0bc7bb1f4a729da6b6717e008fbe93d466f8
aeto80 <guillaume.suyko@gmail.com>
2025-05-04 15:11:02 +0800

    version num

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -98,7 +98,7 @@ function Navbar({ transparent }) {
       >
         {/* Logo */}
         <Tooltip label="Go to BiteBook Home page">
-          Test Run v1.0.1
+          Test Run v1.1.0
           <Link to="/home">
             <Flex alignItems="center">
               <Image
