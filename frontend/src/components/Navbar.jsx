a1e4d7e429c1e4d8c38fb43ce727da5bff35f393
aeto80 <guillaume.suyko@gmail.com>
2025-05-06 03:05:49 +0800

    yes

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -103,7 +103,7 @@ function Navbar({ transparent }) {
           {" "}
           {/* Hide on smaller screens */}
           <Tooltip label="Go to BiteBook Home page">
-            Test Run v1.2.0
+            Test Run v1.2.1
             <Link to="/home">
               <Flex alignItems="center">
                 <Image
